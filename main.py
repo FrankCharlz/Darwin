914654d3f3527079f5a62d28f5acfb50260bb6e2
Frank UDICTI <mjcharlz@gmail.com>
2015-07-17 14:05:44 +0300

    register neary complete

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import urllib
 import jinja2
 import webapp2
 
-from google.appengine.api import users
 from google.appengine.ext import ndb
+from google.appengine.ext import blobstore
 
 
 from models.models import Announcement
 from models.models import User
+from models.models import File
 
 from random import randint
 
@@ -30,33 +31,51 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
 
         users = User.query()
-
         template_values = {
             'number': randint(10,99),
             'users' : users
-
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
+
+def userExists(username):
+    q = User.query(User.name == username)
+    if q>0:
+        return True
+    return False
+
+
+def emailExists(email):
+    q = User.query(User.name == email)
+    if q>0:
+        return True
+    return False
+
+
 class RegisterUser(webapp2.RequestHandler):
-     def post(self):
-         username = self.request.get('username')
-         email = self.request.get('email')
-         pass1 = self.request.get('pass1')
-         pass2 = self.request.get('pass2')
+    def post(self):
+        username = self.request.get('username')
+        email = self.request.get('email')
+        password = self.request.get('pass')
 
-         user = User()
-         user.name = username
-         user.email = email
-         user.password = pass1
-         user.id = randint(100000,999999)
-         user.group_id = 0
+        a = userExists(username)
+        b = emailExists(email)
 
-         key = user.put()
-
-         self.redirect('/')
+        if userExists(username) or emailExists(email):
+            #username or email already in use
+            self.redirect('/register_failed')
+        else:
+            user = User()
+            user.name = username
+            user.email = email
+            user.password = password
+            user.id = randint(100000,999999)
+            user.group_id = 0
+            key = user.put()
+            self.response.write(a,b)
+            self.redirect('/new_user_home')
 
 
 
@@ -66,6 +85,6 @@ class RegisterUser(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/register_user', RegisterUser),
-], debug=True)
+                                  ('/', MainPage),
+                                  ('/register_user', RegisterUser),
+                                  ], debug=True)
--- models/models.py
+++ models/models.py
@@ -6,12 +6,19 @@ class User(ndb.Model):
     name = ndb.StringProperty()
     email = ndb.StringProperty()
     password = ndb.StringProperty()
-    group_id = ndb.IntegerProperty()
     id = ndb.IntegerProperty()
+    group_id = ndb.IntegerProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 class Announcement(ndb.Model):
     user_id =  ndb.IntegerProperty()
     group_id = ndb.IntegerProperty()
     content = ndb.TextProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+class File(ndb.Model):
+    user_id =  ndb.IntegerProperty()
+    group_id = ndb.IntegerProperty()
+    size = ndb.IntegerProperty()
+    file_key = ndb.BlobKeyProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
